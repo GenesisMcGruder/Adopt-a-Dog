c5efb9829efd2cf53899aafa168b94622ff0a46a
Genesis McGruder <genesismcgruder@gmail.com>
2024-04-06 00:34:50 -0400

    reinstated routes in App and updaed indexjs to go along with, created AppContext

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import NavBar from "./NavBar";
-import { Outlet } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import AppContext from "./AppContext";
 
 const App = () => {
+  const [dogs,setDogs] = useState([])
+
+  useEffect(()=>{
+      fetch("http://localhost:3000/dogs")
+      .then((res)=> res.json())
+      .then((data)=> setDogs(data))
+  },[])
+
   return(
-    <>
-    <header>
-      <NavBar />
-    </header>
-    <Outlet />
-    </>
+    <Router>
+      <NavBar/>
+      <AppContext.Provider value={{dogs, setDogs}}>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/Adopt" element={<Adopt dogs={dogs}/>} />
+          <Route path="/ReHome" element={<Rehome setDogs={setDogs}/>} />
+        </Routes>
+      </AppContext.Provider>
+    </Router>
   )
 }
 export default App;
